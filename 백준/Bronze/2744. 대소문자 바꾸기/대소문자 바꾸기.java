a42051d87bd5e037b6b3e7550d2a529d4000e9c9
w-olivine29 <w.olivine29@gmail.com>
2025-06-15 20:00:13 +0900

    [Bronze V] Title: 대소문자 바꾸기, Time: 172 ms, Memory: 17688 KB -BaekjoonHub

--- 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.java
+++ 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.java
@@ -1,24 +1,26 @@
 import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String string = scanner.nextLine();
 
-    public static String change(String str){
-        StringBuilder br = new StringBuilder();
-        for (int i = 0; i < str.length(); i++) {
-           if(Character.isLowerCase(str.charAt(i))) br.append(Character.toUpperCase(str.charAt(i)));
-           else br.append(Character.toLowerCase(str.charAt(i)));
-        }
-       return br.toString();
+        System.out.println(invertCase(string));
+
     }
 
+    public static String invertCase(String str) {
+        StringBuilder sb = new StringBuilder();
 
+        for (char c : str.toCharArray()) {
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String str = sc.nextLine();
-        sc.close();
-
-        System.out.println(change(str));
-
+            String target = String.valueOf(c);
+            if (c >= 97) {
+                sb.append(target.toUpperCase());
+            }else {
+                sb.append(target.toLowerCase());
+            }
+        }
+        return sb.toString();
     }
 }
