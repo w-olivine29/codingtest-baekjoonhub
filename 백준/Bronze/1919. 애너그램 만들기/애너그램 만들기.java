d36ee03fa84ab2ebe81983b5cf4b408e77121b5a
w-olivine29 <w.olivine29@gmail.com>
2025-07-03 00:28:01 +0900

    [Bronze II] Title: 애너그램 만들기, Time: 180 ms, Memory: 17640 KB -BaekjoonHub

--- 백준/Bronze/1919. 애너그램 만들기/애너그램 만들기.java
+++ 백준/Bronze/1919. 애너그램 만들기/애너그램 만들기.java
@@ -1,33 +1,38 @@
 import java.util.Scanner;
 
-//두 단어가 서로 애너그램 관계에 있도록 만들기 위해서 제거해야 하는 최소 개수의 문자 수
 public class Main {
-    public static int solution(String str1, String str2){
-        int cnt = 0;
 
-        int[] a = new int[26]; //알파벳 개수(소문자로 통일한 상태)
-        int[] b = new int[26];
-        for (int i = 0; i < str1.length(); i++)
-            a[str1.charAt(i) - 'a'] ++; //'a' -'a' = 0;
-
-        for (int i = 0; i < str2.length(); i++)
-            b[str2.charAt(i) - 'a'] ++;
-
-
-        for (int i = 0; i < 26; i++) {
-            if(a[i] != b[i])  cnt += Math.abs(a[i] - b[i]);
-        }
-
-        return cnt;
-    }
-
+    static final int ALPHABET_CNT = ('z' - 'a') + 1;
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String str1 = sc.next();
-        String str2 = sc.next();
-        sc.close();
 
-        System.out.println(solution(str1, str2));
+        Scanner scanner = new Scanner(System.in);
+
+        String word1 = scanner.nextLine();
+        String word2 = scanner.nextLine();
+
+        int[] counts1 = getAlphabetCnts(word1);
+        int[] counts2 = getAlphabetCnts(word2);
+
+        System.out.println(getRemoveCnt(counts1, counts2));
+    }
+
+    private static int[] getAlphabetCnts(String word) {
+        int[] counts = new int[ALPHABET_CNT];
+
+        for (int i = 0; i < word.length(); i++) {
+            counts[word.charAt(i) - 'a']++;
+        }
+        return counts;
+    }
+
+    private static int getRemoveCnt(int[] counts1, int[] counts2) {
+        int totalCnt = 0;
+
+        for (int i = 0; i < ALPHABET_CNT; i++) {
+            totalCnt += Math.abs(counts1[i] - counts2[i]);
+        }
+
+        return totalCnt;
     }
 }
