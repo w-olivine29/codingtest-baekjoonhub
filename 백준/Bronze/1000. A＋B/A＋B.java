848501c86dc7ec5f5a90914bb3d3928e840456aa
w-olivine29 <w.olivine29@gmail.com>
2025-07-03 21:27:04 +0900

    [Bronze V] Title: A+B, Time: 168 ms, Memory: 17608 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num1 = Integer.parseInt(scanner.next());
+        int num2 = Integer.parseInt(scanner.next());
+        System.out.println(num1 + num2);
+    }
+}
